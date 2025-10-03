08cf079f008bb95645a4e80483e3c05913aa6f05
Prathmesh Patil <70697030+Grooot266@users.noreply.github.com>
2025-10-04 00:20:42 +0530

    Time: 991 ms (52.04%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,14 +1,13 @@
 /* Write your T-SQL query statement below */
 --Grooot266
-/* Write your T-SQL query statement below */
-With X As
-(SELECT*, ROW_NUMBER() OVER (PARTITION BY customer_id ORDER BY order_date ) as rn 
- FROM Delivery)
-
+WITH CTE_R AS (
+    SELECT *, ROW_NUMBER() OVER(PARTITION BY customer_id ORDER BY order_date)AS RN
+    FROM Delivery
+)
 SELECT ROUND(
-    100.0*SUM(
-        CASE WHEN order_date = customer_pref_delivery_date THEN 1 
-        ELSE 0 
-        END)
-        /COUNT(*),2) as immediate_percentage
-FROM X WHERE rn = 1; 
+    100.0 * SUM(
+    CASE WHEN order_date = customer_pref_delivery_date THEN 1 
+    ELSE 0 END)
+    /COUNT(DISTINCT customer_id),2) AS immediate_percentage
+FROM CTE_R
+WHERE RN = 1
