606d4a05a7aa5f57c8c72a2a5e50c0c48ce6d12e
Prathmesh Patil <70697030+Grooot266@users.noreply.github.com>
2025-10-24 23:13:31 +0530

    Time: 649 ms (14.90%) | Memory: 0B (100.00%) - LeetSync

--- 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/primary-department-for-each-employee.sql
@@ -1,10 +1,6 @@
 /* Write your T-SQL query statement below */
 --Grooot266
 
-WITH CTE AS (
-    SELECT employee_id, department_id, primary_flag, COUNT(*) OVER (PARTITION BY employee_id) AS cnt
-    FROM Employee 
-)
-SELECT employee_id,department_id
-FROM CTE
-WHERE cnt=1 OR primary_flag='Y'
+SELECT DISTINCT Emp.employee_id, COALESCE(Dep.department_id,Emp.department_id) AS department_id 
+FROM Employee AS Emp
+LEFT JOIN Employee AS Dep ON Emp.employee_id = Dep.employee_id AND Dep.primary_flag='Y'
