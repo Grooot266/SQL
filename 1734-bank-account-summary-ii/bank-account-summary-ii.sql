7be33b32a86ee2f8c5e1aca0cdd19efa69716180
Prathmesh Patil <70697030+Grooot266@users.noreply.github.com>
2026-06-12 12:07:18 +0530

    Time: 387 ms (56.88%) | Memory: 0B (100.00%) - LeetSync

--- 1734-bank-account-summary-ii/bank-account-summary-ii.sql
+++ 1734-bank-account-summary-ii/bank-account-summary-ii.sql
@@ -1,8 +1,17 @@
 -- Grooot266
 /* Write your T-SQL query statement below */
 
-SELECT USR.name, SUM(TSR.amount) AS balance
-FROM Users AS USR
-LEFT JOIN Transactions AS TSR ON USR.account = TSR.account
-GROUP BY USR.name
-HAVING SUM(TSR.amount) > 10000
+-- SELECT USR.name, SUM(TSR.amount) AS balance
+-- FROM Users AS USR
+-- LEFT JOIN Transactions AS TSR ON USR.account = TSR.account
+-- GROUP BY USR.name
+-- HAVING SUM(TSR.amount) > 10000
+
+SELECT a.name, a.balance
+FROM (
+    SELECT usr.name, SUM(tsr.amount) AS balance
+    FROM Users AS usr
+    LEFT JOIN Transactions AS tsr ON usr.account = tsr.account 
+    GROUP BY usr.name
+) AS a
+WHERE a.balance > 10000
